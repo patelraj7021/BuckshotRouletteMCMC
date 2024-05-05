aecf95dd70927d8726be4c9086fb50988163313f
Raj <patel.raj.7021@gmail.com>
2024-05-05 15:49:54 -0400

    Merged branches

--- MCMC_implementation.py
+++ MCMC_implementation.py
@@ -19,5 +19,28 @@ def make_deck(num_cards, num_true):
     return deck
 
 
+def sim_game(player_total, dealer_total, num_cards, num_true):
+    
+    deck = make_deck(num_cards, num_true)
+    
+    while player_total != 0 or dealer_total != 0:
+        # do rounds
+    
+    if player_total > dealer_total:
+        winner = True
+    else:
+        winner = False
+    return
 
-print(make_deck(4, 1))
+
+def get_rand_deck():
+    num_card_options = [2, 3, 4, 5, 6, 7, 8]
+    num_cards = random.choice(num_card_options)   
+    num_true_options = list(range(1, num_cards))
+    num_true = random.choice(num_true_options)
+    deck = make_deck(num_cards, num_true)
+
+
+if name == '__main__':
+    
+    
